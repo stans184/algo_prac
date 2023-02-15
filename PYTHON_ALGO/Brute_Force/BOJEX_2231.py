0e7c5a487fdb028bb08409371a319dc4d31a879d
stans184 <life4603@naver.com>
2023-02-15 18:30:03 +0900

    2/15,,,

--- PYTHON_ALGO/Brute_Force/BOJEX_2231.py
+++ PYTHON_ALGO/Brute_Force/BOJEX_2231.py
@@ -2,14 +2,17 @@
 # 자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
 # example input 216, output 198
 
-# 0213 하다맘
 n = int(input())
-ans = 198
+result = 0
 
 for i in range(n):
-    result = i
-
-
-    if result == ans:
-        print(result)
+    # 각 자리수를 뽑아야함
+    # 정수를 문자열로 바꾸고, 그것을 map 시켜서 list 로 변환
+    num_list = list(map(int, str(i)))
+    result = i + sum(num_list)
+    if n == result:
+        print(i)
+        break
+    if i == n-1:
+        print(0)
         break
--- PYTHON_ALGO/binary_search.py
+++ PYTHON_ALGO/binary_search.py
@@ -3,17 +3,23 @@ numList = [12, 25, 31, 48, 54, 66, 70, 83, 95, 108]
 # target = 83, 88
 
 def binary_search(num, targetList):
+    # 가장 왼쪽과
     left = 0
+    # 가장 오른쪽을 잡고
     right = len(targetList)-1
+    # 가운데 값도 잡아줌
     mid = (right + left)//2
     findNum = False
-
+    # search 범위의 왼쪽 값과 오른쪽 값이 바뀌지 않는 한도에서
     while (left <= right):
+        # 만약 가운데 값에 타겟이라면, break
         if targetList[mid] == num:
             findNum = True
             break
+        # 찾는 값이 mid 보다 작다면 mid 왼쪽으로 오른쪽 limit 을 옮김
         elif num < targetList[mid]:
             right = mid-1
+        # 찾는 값이 mid 보다 크다면 mid 오른쪽으로 left 를 옮김
         else:
             left = mid+1
         
