7a0f160d246d612e0e0e91d8bfea62898ba201f0
stans184 <life4603@naver.com>
2023-03-01 13:40:00 +0900

    check PrimeNum

--- PYTHON_ALGO/BOJEX.py
+++ PYTHON_ALGO/BOJEX.py
@@ -1,9 +1,32 @@
+# 3
+# 8           3 5
+# 10          5 5
+# 16          5 11
+
+def isPrime(num):
+    isPNum = True
+    for i in range(2,num):
+        if num%i == 0: isPNum = False
+    return isPNum
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for j in range(2, int(n**0.5) + 1):
+        if n % j == 0:
+            return False
+    return True
+
 t = int(input())
 
-for i in range(t):
-    arr = list(map(int, input().split()))
-    max = 0
-    for num in arr: 
-        if num > max: max = num
+for _ in range(t):
+    n = int(input())
+    a, b = n//2, n//2
 
-    print(f'#{i+1} {max}')
+    while True:
+        if is_prime(a) and is_prime(b) and a + b == n:
+            print(a, b)
+            break
+        else:
+            a -= 1
+            b += 1
