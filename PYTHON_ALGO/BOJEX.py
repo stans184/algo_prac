b6ae3d7af27e6b58370e73ab40c21c1f59ae9fca
stans184 <life4603@naver.com>
2023-03-08 23:33:52 +0900

    하노이탑

--- PYTHON_ALGO/BOJEX.py
+++ PYTHON_ALGO/BOJEX.py
@@ -1,27 +1,17 @@
+# 하노이 탑
+# 3개의 받침대
+# 원판 갯수를 입력받음
+# 몇번 움직였는지 출력하고
+# 뭐가 어디로 가는지 출력해야함
 
-# 3
-# 10 3
-# 1 2 3 4 5 6 7 8 9 10
-# 10 5
-# 6262 6004 1801 7660 7919 1280 525 9798 5134 1821
-# 20 19
-# 3266 9419 3087 9001 9321 1341 7379 6236 5795 8910 2990 2152 2249 4059 1394 6871 4911 3648 1969 2176	 
+def move(no, x, y, arr):
+    if no>1:
+        move(no-1, x, 6-x-y, arr)
+    arr[no] = y
+    if no>1:
+        move(no-1, 6-x-y, y, arr)
 
-import sys
-input = sys.stdin.readline
-
-t = int(input())
-
-for k in range(t):
-    n,m = map(int, input().split())
-    arr = list(map(int, input().split()))
-
-    minValue = max(arr)*m
-    maxValue = 0
-
-    for i in range(n-m+1):
-        result = sum(arr[i:i+m])
-        if result > maxValue: maxValue = result
-        if result < minValue: minValue = result
-
-    print(f'#{k+1} {maxValue-minValue}')
+plate = int(input())
+cnt = dict()
+move(plate, 1, 3, cnt)
+print(cnt)
--- PYTHON_ALGO/BOJEX1316.py
+++ PYTHON_ALGO/BOJEX1316.py
@@ -1,20 +1,22 @@
-n = int(input())
-words = []
-for _ in range(n):
-    words.append(input())
+# from collections import Counter
 
-groupWordCnt = 0
+# n = int(input())
+# arr = [0]*n
 
-# 어떤 알파벳이 나왔는데
-# 바로 다음 순서에서 동일한게 나오면 커서를 그거로 옮기고 다시 검색
-# 만약 다음 순서에서 다르게 나오면 그 뒤에 문자열 중에서 동일한 문자가 있나 검색
-for word in words:
-    checker = True
-    for i in range(len(word)-1):
-        if word[i] == word[i+1]: continue
-        else:
-            for j in range(i+1, len(word)):
-                if word[i] == word[j]: checker = False
-    if checker: groupWordCnt += 1
+# for i in range(n):
+#     arr[i] = input()
 
-print(groupWordCnt)
+# cnt = 0
+
+# for word in arr:
+#     checker = Counter(word)
+#     k = list(checker.keys())
+#     v = list(checker.values())
+#     if len(set(v)) == 1:
+#         cnt += 1
+#     else:
+#         for i in range(len(word)):
+
+
+
+# print(cnt)
