4c6a31ebffd5c623a643675dc98e9840384c4ff1
stans184 <life4603@naver.com>
2023-03-10 21:42:12 +0900

    python useful library

--- PYTHON_ALGO/BOJEX.py
+++ PYTHON_ALGO/BOJEX.py
@@ -4,14 +4,29 @@
 # 몇번 움직였는지 출력하고
 # 뭐가 어디로 가는지 출력해야함
 
-def move(no, x, y, arr):
-    if no>1:
-        move(no-1, x, 6-x-y, arr)
-    arr[no] = y
-    if no>1:
-        move(no-1, 6-x-y, y, arr)
+# def move(no, x, y, arr):
+#     if no>1:
+#         move(no-1, x, 6-x-y, arr)
+#     arr.append([x, y])
+#     if no>1:
+#         move(no-1, 6-x-y, y, arr)
 
-plate = int(input())
-cnt = dict()
-move(plate, 1, 3, cnt)
-print(cnt)
+# plate = int(input())
+# cnt = []
+# move(plate, 1, 3, cnt)
+# print(len(cnt))
+# for content in cnt:
+#     print(content[0], content[1])
+
+import sys, itertools
+
+input = sys.stdin.readline
+
+m,n = map(int, input().split())
+
+ans = list(itertools.combinations_with_replacement(range(1,m+1), n))
+
+for check in ans:
+    for num in check:
+        print(num, end=" ")
+    print()
---
+++ PYTHON_ALGO/UsefulLibrary.py
@@ -0,0 +1,82 @@
+# 1. 내장 함수
+#   > sum, min, max, eval, sorted
+
+# 2. itertools
+#   > permutations
+#   > 모든 경우의 수 계산, 순열
+from itertools import permutations
+data = ['a', 'b', 'c']
+result1 = list(permutations(data,2))
+print(result1)
+# [('a', 'b'), ('a', 'c'), ('b', 'a'), ('b', 'c'), ('c', 'a'), ('c', 'b')]
+#
+#   > combinations
+#   > 순서 없이 가능한 수 조합
+from itertools import combinations
+result2 = list(combinations(data, 2))
+print(result2)
+# [('a', 'b'), ('a', 'c'), ('b', 'c')]
+#
+#   > product
+#   > r 개의 데이터를 뽑아서, 일렬로 나열하는 모든 경우(순열)을 계산
+#   > 원소를 중복해서 뽑는다
+from itertools import product
+result3 = list(product(data, repeat=2))
+print(result3)
+# [('a', 'a'), ('a', 'b'), ('a', 'c'), ('b', 'a'), ('b', 'b'), ('b', 'c'), ('c', 'a'), ('c', 'b'), ('c', 'c')]
+#
+#   > combination with replacement
+#   > 원소를 중복해서 조합
+from itertools import combinations_with_replacement
+result4 = list(combinations_with_replacement(data, 2))
+print(result4)
+# [('a', 'a'), ('a', 'b'), ('a', 'c'), ('b', 'b'), ('b', 'c'), ('c', 'c')]
+
+# 3. heapq
+#   > 파이썬의 heap 은 최소 heap 으로 구성되어 있음
+import heapq
+
+def heapSort(arr):
+    h = []
+    result5 = []
+    for num in arr:
+        heapq.heappush(h, num)
+    for i in range(len(h)):
+        result5.append(heapq.heappop(h))
+    return result5
+
+randomList = [845,132,246,231,584,291,313]
+print(heapSort(randomList))
+result5 = [132, 231, 246, 291, 313, 584, 845]
+
+# 4. bisect
+#   > binarySearch 를 쉽게 할수 있는 라이브러리, 정렬된 배열에서 사용할 떄 효과적
+#   > Time Complexity O(logN)
+from bisect import bisect_left, bisect_right
+print(result5)
+print(bisect_right(result5, 231))       # element 를 1부터 count 가 아니라, 찾는 값의 다음 index 를 반환
+print(result5)
+print(bisect_left(result5, 231))        # element 를 0부터 count
+print(result5)
+
+# 5. collections
+#   > deque
+#   > 자료구조 큐를 구현
+from collections import deque
+data2 = deque(result5)
+print(data2.pop())          # 가장 끝에 들어간 element 제거
+print(data2)                # deque([132, 231, 246, 291, 313, 584])
+print(data2.popleft())      # 가장 처음 들어간 element 제거
+print(data2)                # deque([231, 246, 291, 313, 584])
+#
+#   > Counter
+#   > 반복되는 객체가 주어졌을 때, 몇번씩 등장하는지 count
+from collections import Counter
+data3 = ['red', 'blue','red','green','blue','blue']
+print(Counter(data3))       # Counter({'blue': 3, 'red': 2, 'green': 1})
+
+# 6. math
+#   > factorial
+import math
+print(math.factorial(5))    # 120
+print(math.sqrt(256))       # 16
