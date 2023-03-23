37e01ac6fbb880bc330f90ad85419835bb505cb1
stans184 <life4603@naver.com>
2023-03-23 16:47:22 +0900

    dp, dfs 안됨,,, 다시

--- PYTHON_ALGO/BOJEX.py
+++ PYTHON_ALGO/BOJEX.py
@@ -1,21 +1,32 @@
-# ax + by = c
-# dx + ey = f
+# dfs(V, E, R) {  # V : 정점 집합, E : 간선 집합, R : 시작 정점
+#     visited[R] <- YES;  # 시작 정점 R을 방문 했다고 표시한다.
+#     for each x ∈ E(R)  # E(R) : 정점 R의 인접 정점 집합.(정점 번호를 오름차순으로 방문한다)
+#         if (visited[x] = NO) then dfs(V, E, x);
+# }
 
-# 1 3 -1 4 1 7
-# 2 -1
+def dfs(graph, start, visited=[]):
+    visited.append(graph[start])
+    for node in graph[start]:
+        if node not in visited:
+            dfs(graph, node, visited)
+    return visited
 
-# 2 5 8 3 -4 -11
-# -1 2
+# 첫째 줄에 정점의 수 N (5 ≤ N ≤ 100,000), 간선의 수 M (1 ≤ M ≤ 200,000), 시작 정점 R (1 ≤ R ≤ N)이 주어진다.
+# 다음 M개 줄에 간선 정보 u v가 주어지며 정점 u와 정점 v의 가중치 1인 양방향 간선을 나타낸다.
+# (1 ≤ u < v ≤ N, u ≠ v) 모든 간선의 (u, v) 쌍의 값은 서로 다르다.
 
-# x = (c - by)/a
+# 5 5 1
+# 1 4
+# 1 2
+# 2 3
+# 2 4
+# 3 4
+N, M, R = 5, 5, 1
+graph = [[] for _ in range(N)]
 
-# d*c/a + (e - d*b/a)*y = f
-# (f - d*c/a)/(e-d*b/a) = y
-import sys
-input = sys.stdin.readline
+for _ in range(M):
+    a,b = map(int, input().split())
+    graph[a].append(b)
 
-a,b,c,d,e,f = map(int, input().split())
-
-y = int((f - d*c/a)/(e-d*b/a))
-x = int((c-b*y)/a)
-print(x, y)
+result = dfs(graph, R, visited=[])
+print(result)
