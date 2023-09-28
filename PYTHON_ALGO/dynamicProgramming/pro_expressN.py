f4d000ca66f32e4de6fb127569c24ee0cbab4775
stans184 <life4603@naver.com>
2023-09-28 20:08:51 +0900

    코테연습

--- PYTHON_ALGO/dynamicProgramming/pro_expressN.py
+++ PYTHON_ALGO/dynamicProgramming/pro_expressN.py
@@ -1,5 +1,8 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42895
 
+# 의견
+# https://alreadyusedadress.tistory.com/115
+
 N1 = 5
 number1 = 12
 # 4
@@ -7,6 +10,26 @@ number1 = 12
 N2 = 2
 number2 = 11
 # 3
+
+'''
+n을 1개 써서 만들 수 있는 숫자
+5
+n을 2개 써서 만들 수 있는 숫자
+55, 5+5, 5-5, 5*5, 5/5
+n을 3개 써서 만들 수 있는 숫자
+555,
+55+5, 55-5, 55*5, 55/5
+5+5+5, 5-5-5, 5*5*5, 5/5/5
+
+iteration = 1 -> N 출력
+iteration >= 2      -> 재귀적 진행이 들어가야 하고, 중복이 필요없으니 set 형식으로 하면 좋음
+'N'*(iteration-1)+N
+'N'*(iteration-1)-N
+'N'*(iteration-1)*N
+'N'*(iteration-1)/N
+'''
+
+
 
 def solution(N, number):
     ans = 0
---
+++ PYTHON_ALGO/heap/pro_doublePriorityQueue.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+operation1 = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+# ans [0,0]
+operation2 = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
+# ans [333, -45]
+
